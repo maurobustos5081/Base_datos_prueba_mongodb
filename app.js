7c775e1f351bc8239d6b748b19a763d4fbac932e
maurobustos5081 <mauro_18_b@hotmail.com>
2024-03-18 20:19:16 -0300

    conectando base de datos

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ const uri = require("./atlas_uri")
 
 console.log(uri);
 
+const client = new MongoClient(uri)
+
+const dbname = "bank"
+
+const main = async () => {
+    try {
+        await client.connect();
+        console.log(`Connected to the ${dbname} database`);
+    } catch (err){
+        console.log(`Error connecting to the database: ${err}`);
+    } finally {
+        await client.close();
+    }
+};
+// Run the main function
+
+main();
+
--- atlas_uri.js
+++ atlas_uri.js
@@ -1,2 +1,3 @@
 module.exports = uri = 
-    "mongodb+srv://maurobustos5081:Serginho081@cluster0.23ve4wm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    "mongodb+srv://maurobustos5081:Serginho081@cluster0.23ve4wm.mongodb.net/?retryWrites=true&w=majority";
+
